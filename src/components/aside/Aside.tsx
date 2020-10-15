9a36f14b328f4e2c54aa42d4ba839b56d4c47323
fabiodias <fabio.dias@assistevida.com>
2020-10-15 19:17:51 -0300

    Criação do menu e inclusão da biblioteca react-icons no aside

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import { Container } from './Styles';
+import logoImg from '../../assets/logo.svg'
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp, MdArrowDropUp
+} from 'react-icons/md';
+import {
+  Container,
+  Header,
+  MenuContainer,
+  MenuItemLink,
+  LogImg,
+  Title
+} from './Styles';
 
 const Aside = () => {
   return (
-      <Container>
-          <h1>Aside</h1>
-      </Container>
+    <Container>
+
+      <Header>
+        <LogImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+
+        <MenuItemLink href="#">
+          <MdDashboard />
+              Dachboard
+            </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+              Entradas
+            </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+            Saídas
+            </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+              Sair
+            </MenuItemLink>
+
+      </MenuContainer>
+
+    </Container>
   );
 }
 
